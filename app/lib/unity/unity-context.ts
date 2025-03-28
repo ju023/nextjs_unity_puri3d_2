9704e2885220c38faece919dbe23e4db2917479c
ju023 <dorasan3939@gmail.com>
2025-03-28 22:50:36 +0900

    v2.1_追加:firebaseのセーブデータから座標を取得しゲーム開始時に設定

--- app/lib/unity/unity-context.ts
+++ app/lib/unity/unity-context.ts
@@ -4,9 +4,9 @@ import { useUnityContext } from "react-unity-webgl";
 // useUnityContext を定義する専用のファイル、Unity WebGL ビルド設定を事前定義
 export const useCreateUnityContext = () => {
   return useUnityContext({
-    loaderUrl: "/unity-build/Build/20250326_v2_b6.loader.js",
-    dataUrl: "/unity-build/Build/20250326_v2_b6.data",
-    frameworkUrl: "/unity-build/Build/20250326_v2_b6.framework.js",
-    codeUrl: "/unity-build/Build/20250326_v2_b6.wasm",
+    loaderUrl: "/unity-build/Build/20250328_v2.1_b2.loader.js",
+    dataUrl: "/unity-build/Build/20250328_v2.1_b2.data",
+    frameworkUrl: "/unity-build/Build/20250328_v2.1_b2.framework.js",
+    codeUrl: "/unity-build/Build/20250328_v2.1_b2.wasm",
   });
 };
